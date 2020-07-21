1f2e4e9d2294a6d6c9e83fbeb66890bc2f80fa14
linh-phamdang21 <61008000+linh-phamdang21@users.noreply.github.com>
2020-07-22 03:28:48 +0700

    Add diary-show component

---
+++ src/app/diary/diary-show/diary-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diary-show',
+  templateUrl: './diary-show.component.html',
+  styleUrls: ['./diary-show.component.scss']
+})
+export class DiaryShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
