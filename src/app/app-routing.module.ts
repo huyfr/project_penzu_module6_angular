505b17e735e80004e1a7bf6f2d4bc65cfbbd1d23
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-22 13:14:26 +0700

    Config app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {RegisterComponent} from './auth/register/register.component';
+import {NotActivateTeam} from './auth-guard/not-activate-team';
+import {LoginComponent} from './auth/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', redirectTo: ''},
+  {path: 'register', component: RegisterComponent, canActivate: [NotActivateTeam]},
+  {path: 'login', component: LoginComponent, canActivate: [NotActivateTeam]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
