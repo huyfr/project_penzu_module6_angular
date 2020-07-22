4ef9d36ad3f58afa64ea5557f8b2ac6b0ababf8c
linh-phamdang21 <61008000+linh-phamdang21@users.noreply.github.com>
2020-07-22 07:57:36 +0700

    Add diary-service

---
+++ src/app/model/User.ts
@@ -0,0 +1,19 @@
+// export interface IUser {
+//   id?: number;
+//   avatar: string;
+//   email: string;
+//   name: string;
+//
+//   // id?: number;
+//   // blobstring: string;
+//   // createdate?: string;
+//   // updatedate: string;
+//   // status: number;
+//   // description: string;
+//   // title: string;
+//   // content: string;
+//   // tag;
+//   // reaction;
+//   // user;
+//   // attachment;
+// }
