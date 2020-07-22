8de9b7807d49c4b293dbd7d7a52f7ee05f0a9b0d
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-22 17:27:26 +0700

    Sửa giao diện đăng kí đăng nhập

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../../services/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {SignUpInfo} from '../sign-up-info';
+import {MustMatch} from "../../../util/validate";
 
 @Component({
   selector: 'app-register',
@@ -15,41 +16,53 @@ export class RegisterComponent implements OnInit {
   isSignUpFailed = false;
   errorMessage = '';
   returnUrl: string;
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(64)]),
-    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(64)]),
-    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(128)])
-  });
+  registerForm: FormGroup;
+  submitted = false;
 
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {
   }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(64)]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(64)]),
+      email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(64)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(128)]),
+      confirmPassword: ['', [Validators.required]],
+    }, {
+      validators: MustMatch('password', 'confirmPassword')
+    });
     this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/login';
   }
 
   signUp(): void {
-    const {name, username, email, password} = this.registerForm.value;
-    const signUpInfoForm = new SignUpInfo(name, username, email, password);
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      const {name, username, email, password} = this.registerForm.value;
+      const signUpInfoForm = new SignUpInfo(name, username, email, password);
 
-    this.authService.signUp(signUpInfoForm).subscribe(
-      data => {
-        console.log(data);
-        this.isSignedUp = true;
-        this.isSignUpFailed = false;
-        alert('Register Successful !');
-        this.router.navigateByUrl(this.returnUrl);
-      },
-      error => {
-        console.log(error);
-        this.isSignUpFailed = true;
-      }
-    );
+      this.authService.signUp(signUpInfoForm).subscribe(
+        data => {
+          console.log(data);
+          this.isSignedUp = true;
+          this.isSignUpFailed = false;
+          alert('Register Successful !');
+          this.router.navigateByUrl(this.returnUrl);
+        },
+        error => {
+          console.log(error);
+          this.isSignUpFailed = true;
+        }
+      );
+    }
   }
 
+  get rfc() {
+    return this.registerForm.controls;
+  }
 }
