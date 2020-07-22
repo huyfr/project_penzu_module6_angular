fe74ee8dd4ff6780de94655eec29971bab124dc2
linhhm2406 <linh.hm24@gmail.com>
2020-07-22 16:36:58 +0700

    routing chuc nang admin ->user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {UserListComponent} from './user/userComponent/user-list/user-list.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'user/list', component: UserListComponent},
+  {path: 'user/:id', component: UserListComponent},
+  {path: 'user/block/:id', component: UserListComponent}
 ];
 
 @NgModule({
