3a334e00fd0b8d1798cf907cb769e82fcb1a01bd
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-22 13:12:58 +0700

    Add Auth Guard

---
+++ src/app/auth-guard/can-activate-team.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+
+import {Permissions} from './permissions';
+import {Observable} from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActivateTeam implements CanActivate  {
+  constructor(private permission: Permissions,
+              private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+    if (this.permission.canActivate()) {
+      return this.permission.canActivate();
+    } else {
+      this.router.navigateByUrl('/');
+      return false;
+    }
+  }
+
+}
