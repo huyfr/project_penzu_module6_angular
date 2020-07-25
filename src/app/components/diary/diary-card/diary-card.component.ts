a1738575af1245e74f9a5f1600c30cea38ddcf85
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-25 15:09:48 +0700

    fix delete diary in diary detail

--- src/app/components/diary/diary-card/diary-card.component.ts
+++ src/app/components/diary/diary-card/diary-card.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DiaryService} from '../../../services/diary/diary.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Diary} from "../../../models/Diary";
+import {Diary} from '../../../models/Diary';
 
 
 @Component({
@@ -20,62 +20,72 @@ export class DiaryCardComponent implements OnInit {
   pages: Array<number>;
   constructor(private diaryService: DiaryService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
-  setPage(i, event: any): void{
-    event.preventDefault();
-    this.page = i;
-    this.getAllDiary();
-  }
+  // setPage(i, event: any): void{
+  //   event.preventDefault();
+  //   this.page = i;
+  //   // this.getAllDiary();
+  // }
 
   ngOnInit(): void {
     this.getDiaryById();
-    this.getAllDiaries();
-    this.getAllDiary();
+    // this.getAllDiaries();
+    // this.getAllDiary();
   }
 
-  getAllDiaries(): void{
-    this.diaryService.getAllTitle().subscribe((result) => {
-      this.diaries = result;
-    }, error => {
-    });
-  }
-
-  getDiaryById(): void {
-    const id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.diaryService.getById(+id).subscribe((result) => {
-      this.diary = result;
-    }, error => {
-    });
-  }
-
-  // deleteDiary(id): void {
-  //   const diary = this.diaries[id];
-  //   if (confirm('You want to remove ' + diary.title + '?')) {
-  //     this.diaryService.deleteDiary(id);
-  //     this.router.navigateByUrl('/diaries');
-  //   }
+  // getAllDiaries(): void{
+  //   this.diaryService.getAllTitle().subscribe((result) => {
+  //     this.diaries = result;
+  //   }, error => {
+  //   });
   // }
 
-  getAllDiary(): void{
-    this.diaryService.getAll(this.page).subscribe(
-      list => {
-        console.log(list);
-        this.listDiary = list['content'];
-        this.pages = new Array(list['totalPages']);
-      },
-      error => {
-        console.log(error);
-      }
-    );
+  getDiaryById(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.diaryService.getById(id)
+      .subscribe((result) => {
+      this.diary = result;
+      console.log('getDiary' + this.diary.status);
+    }, error => {
+      this.diary = null;
+    });
   }
-
-  deleteDiary(id): void {
-    const diary = this.listDiary[id];
-    if (confirm('You want to remove ' + diary.title + '?')) {
-      this.diaryService.deleteDiary(id)
-        .subscribe((result) => {
-          this.listDiary = this.listDiary.filter(t => t.id !== diary.id);
+  //
+  deleteDiary(): void {
+    if (confirm('You want to remove ' + this.diary.title + '?')) {
+      this.diaryService.deleteDiary(this.diary.id)
+        .subscribe(res => {
+          res.id !== this.diary.id;
         });
-      this.router.navigateByUrl('/diaries');
+      console.log('deleteDiary' + this.diary.status);
+      this.loadPage();
     }
   }
+
+  loadPage(){
+    this.router.navigate(['journals']);
+  }
+  //
+  // getAllDiary(): void{
+  //   this.diaryService.getAll(this.page).subscribe(
+  //     list => {
+  //       console.log(list);
+  //       this.listDiary = list['content'];
+  //       this.pages = new Array(list['totalPages']);
+  //     },
+  //     error => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
+  //
+  // deleteDiary(i): void {
+  //   const diary = this.listDiary[i];
+  //   if (confirm('You want to remove ' + diary.title + '?')) {
+  //     this.diaryService.deleteDiary(i)
+  //       .subscribe((result) => {
+  //         this.listDiary = this.listDiary.filter(t => t.id !== diary.id);
+  //       });
+  //     this.router.navigateByUrl('/journals');
+  //   }
+  // }
 }
