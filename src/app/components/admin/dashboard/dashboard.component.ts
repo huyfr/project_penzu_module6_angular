bd0cc8820503450ce20f5357d87c4c4714811561
linhhm2406 <linh.hm24@gmail.com>
2020-07-29 17:01:21 +0700

    biểu đồ done

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AdminUserService} from '../../../services/admin/admin-user.service';
 import {Color} from 'ng2-charts';
+import {User} from '../../../models/User';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,8 +12,9 @@ import {Color} from 'ng2-charts';
 export class DashboardComponent implements OnInit {
   reportForm: FormGroup;
   monthChartReportForm: FormGroup;
-  monthSelected: number;
+  monthSelected: string = '01';
   listUserIn2020: [] = [];
+  listUserInMonth: [] = [];
   data2020: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   dataMonth: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -25,6 +27,16 @@ export class DashboardComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true,
     scaleShowValues: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepValue: 1,
+          steps: 1,
+          max: 10,
+        }
+      }]
+    }
   };
   public barChartLabels1 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
   public barChartType1 = 'bar';
@@ -41,6 +53,16 @@ export class DashboardComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true,
     scaleShowValues: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepValue: 1,
+          steps: 1,
+          max: 10,
+        }
+      }]
+    }
   };
   public barChartLabels2 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
     '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
@@ -68,6 +90,9 @@ export class DashboardComponent implements OnInit {
     this.adminUserService.searchByCreateDate(adminReportForm2020).subscribe(
       res => {
         this.listUserIn2020 = res;
+
+        console.log(res[0].createDate.toString().slice(8, 10));
+
         for (let j = 0; j < this.barChartLabels1.length; j++) {
           for (let i = 0; i < res.length; i++) {
             if (this.barChartLabels1[j] == res[i].createDate.toString().slice(5, 7)) {
@@ -80,10 +105,57 @@ export class DashboardComponent implements OnInit {
           {data: this.data2020, label: 'User Qty'},
         ];
 
-        console.log(res);
-      });
 
-    this.monthChartReportForm.valueChanges.subscribe(res => this.monthSelected = res);
+        this.monthSelected = '01';
+        this.dataMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        const temp = [];
+        for (let i = 0; i < res.length; i++) {
+          if (this.monthSelected == res[i].createDate.toString().slice(5, 7)) {
+            temp.push(res[i]);
+          }
+        }
+
+        for (let j = 0; j < this.barChartLabels2.length; j++) {
+          for (let i = 0; i < temp.length; i++) {
+            if (this.barChartLabels2[j] == temp[i].createDate.toString().slice(8, 10)) {
+              this.dataMonth[j] += 1;
+            }
+          }
+        }
+
+        this.barChartData2 = [
+          {data: this.dataMonth, label: 'User Qty'},
+        ];
+
+
+        //khi chọn tháng
+        this.monthChartReportForm.valueChanges.subscribe(res1 => {
+          this.monthSelected = res1;
+          this.dataMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+          const temp = [];
+          for (let i = 0; i < res.length; i++) {
+            if (res1.monthSelection == res[i].createDate.toString().slice(5, 7)) {
+              temp.push(res[i]);
+            }
+          }
+
+          for (let j = 0; j < this.barChartLabels2.length; j++) {
+            for (let i = 0; i < temp.length; i++) {
+              if (this.barChartLabels2[j] == temp[i].createDate.toString().slice(8, 10)) {
+                this.dataMonth[j] += 1;
+              }
+            }
+          }
+
+          this.barChartData2 = [
+            {data: this.dataMonth, label: 'User Qty'},
+          ];
+        });
+      });
   }
 
   report(): void {
