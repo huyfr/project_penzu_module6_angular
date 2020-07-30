f1f4b4fa2e67a262581c4ea3afe7f11a7fae2adc
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-30 13:41:28 +0700

    comment attachment trong album model

--- src/app/models/Album.ts
+++ src/app/models/Album.ts
@@ -1,18 +1,15 @@
 import {Tag} from './Tag';
 import {User} from './User';
-import {Reaction} from './Reaction';
 
 export interface Album {
   id?: number;
   title?: number;
   avatar?: string;
   tag?: Tag;
-  attachment: [];
   user?: User;
-  blobstring?: string;
-  reaction?: Reaction;
   createdate?: string;
   updatedate?: string;
   status?: number;
 }
+
 
