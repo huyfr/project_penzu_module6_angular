67c6c32f665637fda93f42973e4f913fb5576f91
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-07-30 13:35:13 +0700

    viết service thêm album mới, upload ảnh album avatar,upload image

--- src/app/services/album/album.service.ts
+++ src/app/services/album/album.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Album} from '../../models/Album';
 import {Image} from '../../models/Image';
+import {FileForm} from '../../models/file-form';
+import {MultiFileForm} from '../../models/multi-file-form';
 
+const httpOptions: any    = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
-  private sduAlbumUrl = environment.albumUrl;
+  private albumUrl = environment.albumUrl;
   private imageURL = environment.imageUrl;
+  private albumUploadAvatar = environment.albumUploadAvatarUrl;
+  private albumUploadImage = environment.albumUploadImageUrl;
 
   constructor(private http: HttpClient) { }
 
   getAlbumsByUserId(userId: number): Observable<Album[]> {
-    return this.http.get<Album[]>(this.sduAlbumUrl + '/userId/' + userId);
+    return this.http.get<Album[]>(this.albumUrl + '/userId/' + userId);
   }
 
   getListImageByAlbumId(albumId: number): Observable<Image[]> {
@@ -23,6 +35,34 @@ export class AlbumService {
   }
 
   getAlbumById(id: number): Observable<Album> {
-    return this.http.get<Album>(`${this.sduAlbumUrl}/${id}`);
+    return this.http.get<Album>(`${this.albumUrl}/${id}`);
+  }
+
+  createAlbum(album: Album): Observable<Album> {
+    return this.http.post<Album>(this.albumUrl , album);
+  }
+
+  uploadAlbumAvatar(file: FormData, albumId: number): Observable<FileForm> {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+
+    return this.http.post<FileForm>(`${this.albumUploadAvatar}/${albumId}`, file, {headers});
+  }
+
+  uploadAlbumImage(file: FormData, albumId: number): Observable<MultiFileForm[]> {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+
+    return this.http.post<MultiFileForm[]>(`${this.albumUploadImage}/${albumId}`, file , {headers});
+  }
+
+  updateAlbum(album: Album): Observable<Album> {
+    return this.http.put<Album>(this.albumUrl + album.id , album);
+  }
+
+  deleteImageById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.imageURL}/${id}`);
   }
 }
